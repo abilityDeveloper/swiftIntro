3de14653ee95ff78515ddbff96de86884fcae664
Ability Dev <ability585erp@gmail.com>
2022-08-02 14:32:10 -0500

    Setting up basic project structure for Talon's crash course.

--- swiftIntro/Root/swiftIntroApp.swift
+++ swiftIntro/Root/swiftIntroApp.swift
@@ -6,26 +6,16 @@
 //
 
 import SwiftUI
-import FirebaseCore
-
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
-}
-
 
 @main
 struct swiftIntroApp: App {
-    
     //register app delegate for Firebase setup.
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject var session = Session()
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Trunk().environmentObject(session)
         }
     }
 }
