b59ae5644c3acb4ec9207f83fdabbdd607e2a7b4
talon <ability585erp@gmail.com>
2022-08-02 10:52:00 -0500

    Initial Commit

---
+++ swiftIntro/swiftIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftIntroApp.swift
+//  swiftIntro
+//
+//  Created by Tallon Covell on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
